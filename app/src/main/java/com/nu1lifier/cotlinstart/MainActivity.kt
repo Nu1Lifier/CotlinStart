ff5c2eee99fc2a76f5fa8447bd87e41fafd56ec7
George <64532466+Nu1Lifier@users.noreply.github.com>
2021-01-25 17:24:39 +0300

    lesson 3 is done

--- app/src/main/java/com/nu1lifier/cotlinstart/MainActivity.kt
+++ app/src/main/java/com/nu1lifier/cotlinstart/MainActivity.kt
@@ -2,18 +2,20 @@ package com.nu1lifier.cotlinstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private var number1:Int = 6
     private var number2:Int = 10
-    private var text :String = "<1 is here> and <2 is  here>"
+    private var text :String = "<1 is here> and <2 is  here> bla bla <3 here> bla bla <4 here> and <5 here>"
     private var phrase1:String = ""
     private var phrase2:String = ""
     private var tvText1:TextView? = null
     private var tvText2:TextView? = null
 
-
+    //var startNumber = Array(3){""} THIS IS STRING ARRAY
+    var startNumber = IntArray(4){5}
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +28,36 @@ class MainActivity : AppCompatActivity() {
         phrase1 = text.substringAfter('<').substringBefore('>')
         phrase2 = text.substringAfter('<').substringAfter('<').substringBefore('>')
 
+        var counter:Int = 0
+        for (n in text.indices){
+            if (text.get(n) == '<') counter++
+        }
+        var startPosition = IntArray(counter)
+        var endPosition = IntArray(counter)
+        var startPcounter:Int = 0
+        var endPcounter:Int = 0
 
-        tvText1?.setText(phrase1)
-        tvText2.setText(phrase2)
+        for (n in text.indices){
+            if (text.get(n) == '<') {
+                startPosition[startPcounter] = n
+                startPcounter++
+            }
 
+            if (text.get(n) == '>'){
+                endPosition[endPcounter] = n
+                endPcounter++
+            }
+        }
 
+        var textFoundArray = Array(counter){""}
+        for (n in startPosition.indices){
+
+            textFoundArray[n] = text.substring(startPosition[n]+1, endPosition[n])
+            Log.d("MyLog","Text from n: " + textFoundArray[n])
+        }
+
+        tvText1?.text = counter.toString()
+        tvText2.text = phrase2
 
     }
 }
